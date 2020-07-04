0d0cfe9f91498f9dad65da20c8c17be9dc13d842
Payton Gray <paytongray18@gmail.com>
2020-07-04 00:01:04 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -15,7 +15,7 @@ end
 
 introduction_with_language(Payton,Ruby)
 
-def introduction_with_language_optional(name,language = ruby)
+def introduction_with_language_optional(name,language = 'ruby')
  puts "Hi,my name is #{name} and I am learning to program in #{language}."
 end
 
